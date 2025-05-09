eeb9b74832abfb7086b96b7d906ef7444117fe26
achmadray <rayhanachmad84@gmail.com>
2025-05-10 00:43:44 +0800

    main

--- routes/api.php
+++ routes/api.php
@@ -14,20 +14,18 @@ use App\Http\Controllers\Api\BarangMasukController;
 use App\Http\Controllers\Api\BarangKeluarController;
 use App\Http\Controllers\Api\DetailTransaksiController;
 
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-    Route::apiResource('barang', BarangController::class);
-    Route::apiResource('satuan', SatuanController::class);
-    Route::apiResource('pengguna', PenggunaController::class);
-    Route::apiResource('akun', AkunController::class);
-    Route::apiResource('barang_keluar', BarangKeluarController::class);
-    Route::apiResource('barang_masuk', BarangMasukController::class);
-    Route::apiResource('suplier', SuplierController::class);
-    Route::apiResource('transaksi', TransaksiController::class);
-    Route::apiResource('kategori', KategoriController::class);
-    Route::apiResource('menu', MenuController::class);
-    Route::apiResource('detail_transaksi', DetailTransaksiController::class);
-
+Route::apiResource('akun', AkunController::class);
+Route::apiResource('barang', BarangController::class);
+Route::apiResource('satuan', SatuanController::class);
+Route::apiResource('pengguna', PenggunaController::class);
+Route::apiResource('barang_keluar', BarangKeluarController::class);
+Route::apiResource('barang_masuk', BarangMasukController::class);
+Route::apiResource('suplier', SuplierController::class);
+Route::apiResource('transaksi', TransaksiController::class);
+Route::apiResource('kategori', KategoriController::class);
+Route::apiResource('menu', MenuController::class);
+Route::apiResource('detail_transaksi', DetailTransaksiController::class);
