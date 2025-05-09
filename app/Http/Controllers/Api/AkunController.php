21ab2d24537c9d88d049c0bae2d49609393864be
achmadray <rayhanachmad84@gmail.com>
2025-05-10 00:47:54 +0800

    main

--- app/Http/Controllers/Api/AkunController.php
+++ app/Http/Controllers/Api/AkunController.php
@@ -45,32 +45,33 @@ class AkunController extends Controller
     }
 
     public function update(Request $request, $id)
-    {
-        $akun = Akun::find($id);
-        if (!$akun) {
-            return response()->json(['message' => 'Akun tidak ditemukan'], 404);
-        }
-
-        $request->validate([
-            'username' => 'required|unique:akun,username,' . $id . ',id_akun',
-            'password' => 'nullable|min:6',
-            'level' => 'required|in:admin,pegawai,kasir,pengelola_gudang',
-        ]);
-
-        $akun->username = $request->username;
-
-        if ($request->filled('password')) {
-            $akun->password = Hash::make($request->password);
-        }
-
-        $akun->level = $request->level;
-        $akun->save();
-
-        return response()->json([
-            'message' => 'Akun berhasil diperbarui',
-            'data' => $akun
-        ]);
+{
+    $akun = Akun::find($id);
+    if (!$akun) {
+        return response()->json(['message' => 'Akun tidak ditemukan'], 404);
     }
+
+    $request->validate([
+        'username' => 'required|unique:akun,username,' . $id . ',id_akun',
+        'password' => 'nullable|min:6',
+        'level' => 'required|in:admin,pegawai,kasir,pengelola_gudang',
+    ]);
+
+    $akun->username = $request->username;
+
+    if ($request->filled('password')) {
+        $akun->password = Hash::make($request->password);
+    }
+
+    $akun->level = $request->level;
+    $akun->save();
+
+    return response()->json([
+        'message' => 'Akun berhasil diperbarui',
+        'data' => $akun
+    ]);
+}
+
 
     public function destroy($id)
     {
